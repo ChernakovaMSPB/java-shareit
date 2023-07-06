26a2e2658ea7357c15bab2604b84640442993732
Marina <ChernakovaMSPB@yandex.ru>
2023-07-06 12:22:41 +0300

    Техническое задание 15 - корректировка кода по замечаниям ревью

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,11 +1,14 @@
 package ru.practicum.shareit.request;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 /**
@@ -13,6 +16,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping(path = "/requests")
+@Slf4j
 public class ItemRequestController {
     private static final String USER_ID = "X-Sharer-User-Id";
     private ItemRequestService itemRequestService;
@@ -25,24 +29,28 @@ public class ItemRequestController {
     @PostMapping
     public ItemRequestDto create(@Valid @RequestBody ItemRequestDto itemRequestDto,
                                  @RequestHeader(USER_ID) Long userId) {
+        log.info("Создание запросов на вещи с Id: {} и пользователя с Id: {}", itemRequestDto, userId);
         return itemRequestService.create(itemRequestDto, userId);
     }
 
     @GetMapping
     public List<ItemRequestDto> getUserRequests(@NotNull @RequestHeader(USER_ID) Long userId) {
+        log.info("Получение запроса пользователя с Id: {}", userId);
         return itemRequestService.getUserRequests(userId);
     }
 
     @GetMapping("/all")
     public List<ItemRequestDto> getOtherUserRequests(@RequestHeader(USER_ID) Long userId,
-                                                     @RequestParam(defaultValue = "0") int from,
-                                                     @RequestParam(defaultValue = "1") int size) {
+                                                     @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                     @RequestParam(defaultValue = "1") @Positive int size) {
+        log.info("Получение запроса от пользователя с Id: {} на получение всех запросов", userId);
         return itemRequestService.getOtherUserRequests(userId, from, size);
     }
 
     @GetMapping("/{requestId}")
     public ItemRequestDto getRequest(@RequestHeader(USER_ID) Long userId,
                                      @PathVariable Long requestId) {
+        log.info("Получение запроса пользователя с Id: {}", userId);
         return itemRequestService.getRequest(userId, requestId);
     }
 
